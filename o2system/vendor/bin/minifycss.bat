3f8b45015bc89adbaf95b819f3340899704174a0
ayun <ayun@ayun.co>
2016-03-30 15:03:12 +0700

    reinitializing vendor libraries

--- o2system/vendor/bin/minifycss.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-SET BIN_TARGET=%~dp0/../matthiasmullie/minify/bin/minifycss
-php "%BIN_TARGET%" %*
--- o2system/vendor/bin/minifyjs.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-SET BIN_TARGET=%~dp0/../matthiasmullie/minify/bin/minifyjs
-php "%BIN_TARGET%" %*
